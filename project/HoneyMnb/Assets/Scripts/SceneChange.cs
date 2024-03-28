6675560396db2db131c85c4a03d36db10b8eaa7f
rosaa9979 <rosaa9979@gmail.com>
2024-03-29 08:52:12 +0900

    씬 연결 및 팝업 개발

---
+++ project/HoneyMnb/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Change()
+    {
+        switch (gameObject.name)
+        {
+            case "login_button":
+                SceneManager.LoadScene("Camera");
+                break;
+            case "game_start_button":
+                SceneManager.LoadScene("Game");
+                break;
+
+            case "title_button":
+                SceneManager.LoadScene("Title");
+                break;
+
+            case "retry_button":
+                SceneManager.LoadScene("Game");
+                break;
+
+            default:
+                Debug.LogWarning("No matching scene found for object name: " + gameObject.name);
+                break;
+        }
+    }
+}
