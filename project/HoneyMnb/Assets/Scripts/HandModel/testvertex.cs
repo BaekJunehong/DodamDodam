61941186bbcb54934f5e4b9acbd004c661f38c2e
WooHyuk-Lee <00woohyuk@naver.com>
2024-04-14 23:00:00 +0900

    add func

--- project/HoneyMnb/Assets/Scripts/HandModel/testvertex.cs
+++ project/HoneyMnb/Assets/Scripts/HandModel/testvertex.cs
@@ -15,8 +15,19 @@ public class testvertex : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 test_data = _handtracker.GetVertex(4);
-        // transform.position = test_data;
+        Vector3 test_data = _handtracker.GetCenter;
         Debug.Log(test_data);
+        // Debug.Log(_handtracker.GetValidHand);
+        // Vector3 test_data = _handtracker.GetVertex(12);
+        //Debug.Log(test_data);
+        // bool test_hold = _handtracker.IsHold();
+        // Debug.Log(test_hold);
+        // int test_angle = _handtracker.Cutting();
+        // if ( test_angle != -1){
+        //     Debug.Log(test_angle);
+        // }
+        // Debug.Log(test_angle);
+        // Vector3 testt = _handtracker.GetDirection();
+        // Debug.Log(testt);
     }
 }
