ce4e44c91af25584c151981ec8820749428b7718
KTH <taeho0708@naver.com>
2024-05-02 14:53:49 +0900

    회원가입 시 팝업 안 닫히는 오류 수정

--- project/HoneyMnb/Assets/Scripts/GameControl/loginRegister.cs
+++ project/HoneyMnb/Assets/Scripts/GameControl/loginRegister.cs
@@ -13,10 +13,11 @@ public class loginRegister : MonoBehaviour
 
     void Start()
     {
-        // 서버의 IP 주소와 포트 번호를 입력합니다.
+        // 서버의 IP 주소와 포트 번호를 입력
         string serverIP = "35.216.33.115";
         int serverPort = 3000;
 
+        // TcpClient에서 서버로 소켓 연결
         try {
             client = new TcpClient(serverIP, serverPort);
         } catch (Exception e) {
@@ -24,6 +25,7 @@ public class loginRegister : MonoBehaviour
         }
     }
 
+    // 로그인 팝업에서 로그인 버튼 클릭 시 로그인 기능 수행
     public void OnLoginButtonClicked()
     {
         string username = GameObject.Find("ID_inputField").GetComponent<TMP_InputField>().text;
@@ -38,6 +40,7 @@ public class loginRegister : MonoBehaviour
         read_stream.Close();
     }
 
+    // 회원가입 팝업에서 회원가입 버튼 클릭 시 회원가입 기능 수행
     public void OnSignUpButtonClicked()
     {
         string username = GameObject.Find("newID_inputField").GetComponent<TMP_InputField>().text;
@@ -54,6 +57,7 @@ public class loginRegister : MonoBehaviour
         read_stream.Close();
     }
 
+    // 서버로 로그인 혹은 회원가입 데이터 전송
     private void SendData(NetworkStream write_stream, string command, string username, string password, string name = "", string birthdate = "")
     {
         if (client == null || !client.Connected) {
@@ -72,6 +76,7 @@ public class loginRegister : MonoBehaviour
         }
     }
 
+    // 서버로부터 로그인 혹은 회원가입 결과 메시지 수신 및 처리
     public void ReadData(NetworkStream read_stream)
     {
         if (client == null || !client.Connected) {
@@ -88,7 +93,7 @@ public class loginRegister : MonoBehaviour
 
             if (message == "Login successful\n") {
                 SceneManager.LoadScene("Success");
-            } else if (message == "Sign up successful\n") {
+            } else if (message == "User created successfully\n") {
                 GameObject.Find("signup_popup").SetActive(false);
             }
         } catch (SocketException s) {
@@ -96,6 +101,7 @@ public class loginRegister : MonoBehaviour
         }
     }
 
+    // 애플리케이션 종료 시 소켓 연결 종료
     private void OnApplicationQuit() {
         if (client != null) {
             client.Close();
