8f2e5a728c8f7a42588da9a762b7eda3e24b3c73
koo4802 <koo4802@gmail.com>
2024-05-18 13:34:41 +0900

    Implement Coroutine for delay

--- DodamDodam/Assets/Scripts/tutorial.cs
+++ DodamDodam/Assets/Scripts/tutorial.cs
@@ -37,8 +37,8 @@ public class tutorial : MonoBehaviour
     private hand Hand;
     private HandTracker _handtracker;
     private LineRenderer cuttedLineRenderer;
-    private float time = 0;
     private audioManager audioScript;
+    private bool isCoroutineRunning = false;
 
     void Start()
     {
@@ -56,6 +56,7 @@ public class tutorial : MonoBehaviour
                 }
             };
         }
+        scissorsObject.transform.rotation = HandSide.HS == whichSide.right ? Quaternion.Euler(0, 0, 90) : Quaternion.Euler(0, 0, 270);
         cuttedPointObject.transform.position = HandSide.HS == whichSide.right ? new Vector3(7, 0, 0) : new Vector3(-7, 0, 0);
         destinationObject.transform.position = HandSide.HS == whichSide.right ? new Vector3(1, 0, 0) : new Vector3(-1, 0, 0);
 
@@ -78,15 +79,56 @@ public class tutorial : MonoBehaviour
             scissorsObject.transform.position = Vector3.MoveTowards(scissorsObject.transform.position, handObject.transform.position, moveSpeed * Time.deltaTime);
         }
     }
-    private bool timeSleep(float timeLimit){
-        time += Time.deltaTime;
-            if(time >= timeLimit){
-                time = 0;
-                return true;
-            }
-            else{
-                return false;
-            }
+
+    IEnumerator guideLine(){
+        isCoroutineRunning  = true;
+        yield return new WaitForSecondsRealtime(3f);
+        audioScript.changeAudio(5);
+        instructionText.text = "6. 분홍색 점을 향해 가위질을 해보세요.";
+        print("분홍색 점을 향해 가위질을 해보세요");
+        destinationObject.SetActive(true);
+        step = 5;
+        isCoroutineRunning = false;
+    }
+
+    IEnumerator endScene(){
+        isCoroutineRunning  = true;
+        yield return new WaitForSecondsRealtime(0.5f);
+        Destroy(handObject);
+        Destroy(scissorsObject);
+        Destroy(cuttedPointObject);
+        Destroy(guideLineObjcet);
+        Destroy(destinationObject);
+        Destroy(websource);
+        isCoroutineRunning = false;
+
+        SceneManager.LoadScene("Camera");
+    }
+    IEnumerator scissors(int power){
+        isCoroutineRunning = true;
+        yield return new WaitForSecondsRealtime(1f);
+        
+        cuttedLineRenderer.positionCount = 2;
+        cuttedLineRenderer.SetPosition(0, cuttedPointObject.transform.position);
+        cuttedPointObject.transform.position = HandSide.HS == whichSide.right ? cuttedPointObject.transform.position + Vector3.left * 3 : cuttedPointObject.transform.position + Vector3.right * 3;
+        cuttedLineRenderer.SetPosition(1, cuttedPointObject.transform.position);
+        //성공을 출력
+        step = 3;
+        audioScript.changeAudio(3);
+        instructionText.text = "4. 가위를 분홍색 점에 가져가보세요.";
+        print("4. 가위를 분홍색 점에 가져가보세요!");
+        
+        isCoroutineRunning = false;
+    }
+    IEnumerator strongScissors(Vector3 direction){
+        isCoroutineRunning = true;
+        yield return new WaitForSecondsRealtime(1f);
+        cuttedLineRenderer.positionCount++;
+        cuttedPointObject.transform.position += direction * 6;
+        cuttedLineRenderer.SetPosition(cuttedLineRenderer.positionCount - 1, cuttedPointObject.transform.position);
+        //성공을 출력
+        step = -1;
+        isCoroutineRunning = false;
     }
 
     private void gameFlow(){
@@ -96,14 +138,9 @@ public class tutorial : MonoBehaviour
             case -1:
                 audioScript.changeAudio(8);
                 instructionText.text = "성공!";
-                Destroy(handObject);
-                Destroy(scissorsObject);
-                Destroy(cuttedPointObject);
-                Destroy(guideLineObjcet);
-                Destroy(destinationObject);
-                Destroy(websource);
-
-                SceneManager.LoadScene("Camera");
+                if(!isCoroutineRunning){
+                    StartCoroutine(endScene());
+                }
             break;
             case 0:
             //손으로 가위를 잡아야 하는 단계
@@ -124,21 +161,13 @@ public class tutorial : MonoBehaviour
                 step = 2;
                 audioScript.changeAudio(2);
                 instructionText.text = "3. 가위질을 해보세요.";
-                print("가위질을 해보세요");
+                print("3. 가위질을 해보세요");
             }
             break;
             case 2:
             //가위질을 해보세요(가위가 멈춤)
-            if(power >= 1 && isGrabbedScissors){
-                cuttedLineRenderer.positionCount = 2;
-                cuttedLineRenderer.SetPosition(0, cuttedPointObject.transform.position);
-                cuttedPointObject.transform.position = HandSide.HS == whichSide.right ? cuttedPointObject.transform.position + Vector3.left * 3 : cuttedPointObject.transform.position + Vector3.right * 3;
-                cuttedLineRenderer.SetPosition(1, cuttedPointObject.transform.position);
-                //성공을 출력
-                step = 3;
-                audioScript.changeAudio(3);
-                instructionText.text = "4. 가위를 분홍색 점에 가져가보세요.";
-                print("가위를 분홍색 점에 가져가보세요!");
+            if(power >= 1 && isGrabbedScissors && !isCoroutineRunning){
+                StartCoroutine("scissors", power);
             }
             break;
             case 3:
@@ -149,18 +178,14 @@ public class tutorial : MonoBehaviour
                 step = 4;
                 audioScript.changeAudio(4);
                 instructionText.text = "5. 손을 움직여 방향을 조절해보세요.";
-                print("손을 움직여 방향을 조절해보세요!");
+                print("5. 손을 움직여 방향을 조절해보세요!");
             }
             break;
             case 4:
             //손을 움직여 방향을 조절해보세요!
             guideLineObjcet.SetActive(true);
-            if(timeSleep(3)){
-                step = 5;
-                audioScript.changeAudio(5);
-                instructionText.text = "6. 분홍색 점을 향해 가위질을 해보세요.";
-                print("분홍색 점을 향해 가위질을 해보세요");
-                destinationObject.SetActive(true);
+            if(!isCoroutineRunning){
+                StartCoroutine(guideLine());
             }
             break;
             case 5:
@@ -173,7 +198,7 @@ public class tutorial : MonoBehaviour
                 step = 6;
                 audioScript.changeAudio(6);
                 instructionText.text = "7. 가위를 분홍색 점에 가져가보세요.";
-                print("가위를 분홍색 점에 가져가보세요!");
+                print("7. 가위를 분홍색 점에 가져가보세요!");
                 destinationObject.SetActive(false);
             }
             break;
@@ -185,17 +210,13 @@ public class tutorial : MonoBehaviour
                 step = 7;
                 audioScript.changeAudio(7);
                 instructionText.text = "8. 강하게 가위질해보세요.";
-                print("강하게 가위질해보세요!");
+                print("8. 강하게 가위질해보세요!");
             }
             break;
             case 7:
             //강하게 가위질해보세요!
-            if(power >= 2 && isGrabbedScissors){
-                cuttedLineRenderer.positionCount++;
-                cuttedPointObject.transform.position += direction * 6;
-                cuttedLineRenderer.SetPosition(cuttedLineRenderer.positionCount - 1, cuttedPointObject.transform.position);
-                //성공을 출력
-                step = -1;
+            if(power >= 2 && isGrabbedScissors && !isCoroutineRunning){
+                StartCoroutine("strongScissors", direction);
             }
             
             break;
