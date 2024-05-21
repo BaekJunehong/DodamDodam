2cae7c732d7ea1c2e68f8b122b168a0cb4aee11f
송준석 <rosaa9979@gmail.com>
2024-05-21 18:50:49 +0900

    Result 씬 ui 개선

--- DodamDodam/Assets/Scripts/RseultController.cs
+++ DodamDodam/Assets/Scripts/RseultController.cs
@@ -7,11 +7,13 @@ public class ResultController : MonoBehaviour
 {
     public TextMeshProUGUI score_text;
     public TextMeshProUGUI error_text;
+    public TextMeshProUGUI name_text;
     // Start is called before the first frame update
     void Start()
     {
         score_text.text = Timer.T.ToString("N2");
         error_text.text = ErrorCount.errorCount.ToString("0");
+        name_text.text = NetClient.getName;
     }
 
     // Update is called once per frame
